d4c6ec9a2642937237fa14e5a167be5b3d05c696
Joseph Wasden <jpwasden93@gmail.com>
2024-12-04 09:36:00 +0000

    Finished day 3

--- day_3/main.py
+++ day_3/main.py
@@ -1,11 +1,9 @@
+from pathlib import Path
 
-
-def main():
+def mul_no_conditionals(corrupted_memory) -> int:
     """Purposefully done without regex"""
 
-    corrupted_memory = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
     valid_muls = []
-
     for i in range(len(corrupted_memory)):
         if corrupted_memory[i: i+4] != "mul(":
             continue
@@ -34,7 +32,54 @@ def main():
             if valid:
                 valid_muls.append((int(first), int(second)))
 
-    print(sum(x * y for x, y in valid_muls))
+    return sum(x * y for x, y in valid_muls)
+
+
+def mul(corrupted_memory: str) -> int:
+    """Purposefully done without regex"""
+    mul_enabled = True
+    valid_muls = []
+    for i in range(len(corrupted_memory)):
+        if corrupted_memory[i: i + 4] == "do()":
+            mul_enabled = True
+        elif corrupted_memory[i: i + 7] == "don't()":
+            mul_enabled = False
+
+        if corrupted_memory[i: i + 4] == "mul(" and mul_enabled:
+            first = ""
+            second = ""
+            constructing_first = True
+            valid = False
+            for x in range(i + 4, i + 13):
+                char = corrupted_memory[x]
+                if char.isnumeric():
+                    if constructing_first:
+                        first += char
+                    else:
+                        second += char
+                elif char == "," and constructing_first:
+                    constructing_first = False
+                elif char == ")" and not constructing_first:
+                    if first and second:
+                        valid = True
+
+                    break
+                else:
+                    break
+
+            if valid:
+                valid_muls.append((int(first), int(second)))
+
+    return sum(x * y for x, y in valid_muls)
+
+
+def main():
+    corrupted_memory = Path("input.txt").read_text()
+    answer = mul(corrupted_memory)
+    print(answer)
+
+
+
 
 
 
